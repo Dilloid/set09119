75128d15529c76e0f837c080ae23db409a5186f0
Dillon Aitken <dillonaitken87@gmail.com>
2022-09-23 13:49:53 +0100

    Up to aero drag

--- code/02_particles_framework/PhysicsEngine.cpp
+++ code/02_particles_framework/PhysicsEngine.cpp
@@ -18,6 +18,8 @@ void SymplecticEuler(vec3& pos, vec3& vel, float mass, const vec3& accel, const 
 {
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// TODO: Implement
+	vel += dt * accel + (impulse/mass);
+	pos += dt * vel;
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
 
@@ -26,6 +28,40 @@ vec3 CollisionImpulse(Particle& pobj, const glm::vec3& cubeCentre, float cubeHal
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// TODO: Calculate collision impulse
 	vec3 impulse{ 0.0f };
+	vec3 floorN{ 0.0f };
+
+	if (pobj.Position().y <= (cubeCentre.y - cubeHalfExtent)) {
+		pobj.SetPosition(vec3{ pobj.Position().x, (cubeCentre.y - cubeHalfExtent), pobj.Position().z });
+		floorN = { 0.0f, 1.0f, 0.0f };
+	}
+
+	if (pobj.Position().y >= (cubeCentre.y + cubeHalfExtent)) {
+		pobj.SetPosition(vec3{ pobj.Position().x, (cubeCentre.y + cubeHalfExtent), pobj.Position().z });
+		floorN = { 0.0f, -1.0f, 0.0f };
+	}
+	
+	if (pobj.Position().x <= (cubeCentre.x - cubeHalfExtent)) {
+		pobj.SetPosition(vec3{ (cubeCentre.x - cubeHalfExtent), pobj.Position().y, pobj.Position().z });
+		floorN = { -1.0f, 0.0f, 0.0f };
+	}
+	
+	if (pobj.Position().x >= (cubeCentre.x + cubeHalfExtent)) {
+		pobj.SetPosition(vec3{ (cubeCentre.x + cubeHalfExtent), pobj.Position().y, pobj.Position().z });
+		floorN = { 1.0f, 0.0f, 0.0f };
+	}
+	
+	if (pobj.Position().z <= (cubeCentre.z - cubeHalfExtent)) {
+		pobj.SetPosition(vec3{ pobj.Position().x, pobj.Position().y, (cubeCentre.z - cubeHalfExtent) });
+		floorN = { 0.0f, 0.0f, -1.0f };
+	}
+
+	if (pobj.Position().z >= (cubeCentre.z + cubeHalfExtent)) {
+		pobj.SetPosition(vec3{ pobj.Position().x, pobj.Position().y, (cubeCentre.z + cubeHalfExtent) });
+		floorN = { 0.0f, 0.0f, 1.0f };
+	}
+	
+	float vClose = dot(pobj.Velocity(), floorN);
+	impulse = -(1 + coefficientOfRestitution) * pobj.Mass() * vClose * floorN;
 	return impulse;
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
@@ -49,7 +85,7 @@ void PhysicsEngine::Init(Camera& camera, MeshDb& meshDb, ShaderDb& shaderDb)
 	meshDb.Add("cube", Mesh(MeshDataFromWavefrontObj("resources/models/cube.obj")));
 	meshDb.Add("sphere", Mesh(MeshDataFromWavefrontObj("resources/models/sphere.obj")));
 	meshDb.Add("cone", Mesh(MeshDataFromWavefrontObj("resources/models/cone.obj")));
-	auto mesh = meshDb.Get("cube");
+	auto mesh = meshDb.Get("sphere");
 
 	// Initialise ground
 	ground.SetMesh(groundMesh);
@@ -62,7 +98,7 @@ void PhysicsEngine::Init(Camera& camera, MeshDb& meshDb, ShaderDb& shaderDb)
 	particle.SetColor(vec4(1, 0, 0, 1));
 	particle.SetPosition(vec3(0, 5, 0));
 	particle.SetScale(vec3(0.1f));
-	particle.SetVelocity(vec3(1.f, 0.0f, 2.f));
+	particle.SetVelocity(vec3(1.f, 0.0f, 5.f));
 
 	camera = Camera(vec3(0, 2.5, 10));
 
@@ -79,7 +115,13 @@ void PhysicsEngine::Update(float deltaTime, float totalTime)
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	// TODO: Implement a simple integration scheme
 	vec3 p = particle.Position(), v = particle.Velocity();
-	vec3 acceleration = vec3(0.0f);
+	vec3 fGravity = particle.Mass() * GRAVITY;
+
+	float vMag = sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2));
+	// REPLACE 
+	vec3 fAero = 0.5f * 1.0f * glm::length(v) * 0.47f * (3.14f * (0.05f * 0.05f)) * (-v);
+	vec3 acceleration = (fGravity + fAero) / particle.Mass();
+
 	SymplecticEuler(p,v, particle.Mass(), acceleration, impulse, deltaTime);
 	particle.SetPosition(p);
 	particle.SetVelocity(v);
